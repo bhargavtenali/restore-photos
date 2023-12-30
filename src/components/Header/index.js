bb02fd77ae3420eaecdc569f4ee23943b2835673
Bhargav Tenali <t.bhargav@kayrros.com>
2023-12-31 01:25:41 +0530

    errors fixed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className="header-contianer">
       <div className="is-flex is-flex-direction-row is-justify-content-space-between is-align-items-center px-4 py-5">
-        <Link smooth to="/" className="is-flex is-align-items-flex-end">
+        <Link to="/" className="is-flex is-align-items-flex-end">
           <img className="header-logo" src={headerLogo} alt="header-logo" />
           <h1 className="ml-2 is-size-4-touch is-size-2 has-text-weight-bold has-text-black">
             restorePhotos.io
